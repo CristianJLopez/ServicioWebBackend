f1b518642695011581be8791ee02a0d527eeb8a7
CristianJLopez <cjlopez60@misena.edu.co>
2021-03-11 17:54:33 -0500

    Package Implementacion y services actualizada

--- Proyecto/src/main/java/com/salle/implementacion/ClienteServices.java
+++ Proyecto/src/main/java/com/salle/implementacion/ClienteServices.java
@@ -1,11 +1,28 @@
 package com.salle.implementacion;
+
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.salle.dao.IUsuarioDao;
 import com.salle.entities.Usuario;
 import com.salle.services.IUsuarioServices;
-public class ClienteServices {
 
-}
+@service
+public class ClienteServices implements IUsuarioServices {
+	
+	@Autowired
+	private IUsuarioDao clienteDao;
+	@Transactional(readOnly = true)
+	public List<Usuario> findAll() {
+	return (List<Usuario>) clienteDao.findAll();
+	}
+	@Transactional(readOnly = false)
+	public Usuario create(Usuario usuario){
+	System.out.println(usuario.getDependencia());
+	return clienteDao.save(usuario);
+	}
+	}
+
